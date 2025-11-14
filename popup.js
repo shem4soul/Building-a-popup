6e6e27a2303fa9a2fe6cf903d9a5e3d57eec5690
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-14 10:13:32 +0100

    fix a bug

--- popup.js
+++ popup.js
@@ -1,3 +1,7 @@
 const button = document.querySelector('button');
 const popup = document.querySelector('.popup-wrapper');
 const close = document.querySelector('.popup-close');
+
+button.addEventListener('click', () => {
+    popup.style.display = 'block';
+});
